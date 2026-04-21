3136464ac8b369b0dd29f9eaee8de902e8a0b3da
AlexandreLopes02 <alenovoxio@gmail.com>
2026-04-21 09:52:36 -0300

    feat: implementa melhoria de cominicação do front com .api

--- src/app/features/cadastros/cargas/carga.service.ts
+++ src/app/features/cadastros/cargas/carga.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/core/api.service';
 import { Carga } from './carga.model';
 
 @Injectable({ providedIn: 'root' })
 export class CargaService {
-  private url = 'http://localhost:3001/cargas';
+  private url = `${this.api.baseUrl}/cargas`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private api: ApiService) {}
 
   list(): Observable<Carga[]> {
     return this.http.get<Carga[]>(this.url);
